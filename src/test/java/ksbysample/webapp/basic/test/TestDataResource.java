9f74a7084b9177a68af04592e4be320a58d00b56
ksby <kusa@db3.so-net.ne.jp>
2015-02-08 02:47:45 +0900

    Issue #25 に対応しました。ログイン画面のテストクラスを作成しました。

---
+++ src/test/java/ksbysample/webapp/basic/test/TestDataResource.java
@@ -0,0 +1,71 @@
+package ksbysample.webapp.basic.test;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.csv.CsvDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.rules.ExternalResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@Component
+public class TestDataResource extends ExternalResource {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private File backupFile;
+
+    @Override
+    protected void before() throws Throwable {
+        IDatabaseConnection conn = null;
+        try {
+            conn = new DatabaseConnection(dataSource.getConnection());
+
+            // バックアップを取得する
+            QueryDataSet partialDataSet = new QueryDataSet(conn);
+            partialDataSet.addTable("user");
+            backupFile = File.createTempFile("world_backup", "xml");
+            FlatXmlDataSet.write(partialDataSet, new FileOutputStream(backupFile));
+
+            // テストデータに入れ替える
+            IDataSet dataset = new CsvDataSet(new File("src/test/resources/testdata"));
+            DatabaseOperation.CLEAN_INSERT.execute(conn, dataset);
+        }
+        finally {
+            if (conn != null) conn.close();
+        }
+    }
+
+    @Override
+    protected void after() {
+        try {
+            IDatabaseConnection conn = null;
+            try {
+                conn = new DatabaseConnection(dataSource.getConnection());
+
+                // バックアップからリストアする
+                if (backupFile != null) {
+                    IDataSet dataSet = new FlatXmlDataSetBuilder().build(backupFile);
+                    DatabaseOperation.CLEAN_INSERT.execute(conn, dataSet);
+                    backupFile.delete();
+                    backupFile = null;
+                }
+            }
+            finally {
+                if (conn != null) conn.close();
+            }
+        }
+        catch (Exception ignored) {
+        }
+    }
+
+}
--- src/test/java/ksbysample/webapp/basic/web/LoginControllerTest.java
+++ src/test/java/ksbysample/webapp/basic/web/LoginControllerTest.java
@@ -1,17 +1,31 @@
 package ksbysample.webapp.basic.web;
 
 import ksbysample.webapp.basic.Application;
+import ksbysample.webapp.basic.test.TestDataResource;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Filter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,11 +37,20 @@ public class LoginControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    @Rule
+    @Autowired
+    public TestDataResource testDataResource;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
     private MockMvc mvc;
 
     @Before
-    public void setUp() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    public void setUp() throws Exception {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .addFilter(springSecurityFilterChain)
+                .build();
     }
 
     @Test
@@ -38,4 +61,45 @@ public class LoginControllerTest {
                 .andExpect(xpath("/html/head/title").string("ログイン"));
     }
 
+    @Test
+    public void testLogin() throws Exception {
+        // 存在しないユーザでログインを試みる
+        this.mvc.perform(formLogin()
+                        .user("id", "test")
+                        .password("password", "ptest")
+        )
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated())
+                .andExpect(request().sessionAttribute("SPRING_SECURITY_LAST_EXCEPTION", isA(BadCredentialsException.class)));
+
+        // 使用できないユーザ ( enabled = 0 ) でログインを試みる
+        this.mvc.perform(formLogin()
+                        .user("id", "suzuki")
+                        .password("password", "20140101")
+        )
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated())
+                .andExpect(request().sessionAttribute("SPRING_SECURITY_LAST_EXCEPTION", isA(DisabledException.class)));
+
+        // ログイン可能なユーザでログインする
+        this.mvc.perform(formLogin()
+                        .user("id", "tanaka")
+                        .password("password", "tarou")
+        )
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/countryList"))
+                .andExpect(authenticated().withUsername("tanaka"));
+    }
+
+    @Test
+    public void testEncode() throws Exception {
+        MvcResult result = this.mvc.perform(get("/encode?password=ptest")).andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andReturn();
+        String crypt = result.getResponse().getContentAsString();
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        assertThat(passwordEncoder.matches("ptest", crypt), is(true));
+    }
 }
