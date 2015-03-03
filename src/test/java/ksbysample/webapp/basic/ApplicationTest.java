def7e359f889d46610291e202df977c73b665029
ksby <kusa@db3.so-net.ne.jp>
2015-03-04 02:26:40 +0900

    Issue #37 に対応しました。Spring Boot のバージョンを 1.2.2 へ上げました。

--- src/test/java/ksbysample/webapp/basic/ApplicationTest.java
+++ src/test/java/ksbysample/webapp/basic/ApplicationTest.java
@@ -1,39 +1,53 @@
 package ksbysample.webapp.basic;
 
+import org.junit.Ignore;
 import org.junit.Rule;
+import org.junit.Test;
 import org.springframework.boot.test.OutputCapture;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore
 public class ApplicationTest {
 
     @Rule
     public OutputCapture outputCapture = new OutputCapture();
 
-/*
-    @Test(expected = Exception.class)
+    @Test
     public void testApplicationProductMode() {
+        String SPRING_PROFILES_ACTIVE = System.getProperty("spring.profiles.active");
         System.setProperty("spring.profiles.active", "product");
-        Application.main(new String[] {"--server.port=8081"});
+        Application.main(new String[]{"--server.port=8081"});
         String output = this.outputCapture.toString();
-        assertTrue(output, output.contains("Started Application"));
-        System.setProperty("spring.profiles.active", "");
+        assertThat(output.contains("Started Application"), is(true));
+        System.setProperty("spring.profiles.active", SPRING_PROFILES_ACTIVE);
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testApplicationDevelopMode() {
+        String SPRING_PROFILES_ACTIVE = System.getProperty("spring.profiles.active");
         System.setProperty("spring.profiles.active", "develop");
         Application.main(new String[]{"--server.port=8082"});
         String output = this.outputCapture.toString();
-        assertTrue(output, output.contains("Started Application"));
-        System.setProperty("spring.profiles.active", "");
+        assertThat(output.contains("Started Application"), is(true));
+        System.setProperty("spring.profiles.active", SPRING_PROFILES_ACTIVE);
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testApplicationEmptyMode() {
-        Application.main(new String[]{"--server.port=8083"});
+        String SPRING_PROFILES_ACTIVE = System.getProperty("spring.profiles.active");
+        System.setProperty("spring.profiles.active", "");
+        try {
+            Application.main(new String[]{"--server.port=8083"});
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         String output = this.outputCapture.toString();
-        assertFalse(output, output.contains("Started Application"));
-        assertTrue(output, output.contains("JVMの起動時引数 -Dspring.profiles.active で develop か product を指定して下さい"));
+        assertThat(output.contains("Started Application"), is(false));
+        assertThat(output.contains("JVMの起動時引数 -Dspring.profiles.active で develop か product を指定して下さい"), is(true));
+        System.setProperty("spring.profiles.active", SPRING_PROFILES_ACTIVE);
     }
-*/
 
 }
